f78649eec5afb93a398a8dcf52fbe8129da139d9
123 <qeqw@eqwdas.com>
2021-04-08 21:59:35 +0300

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+import 'focus-visible'
+import lazyImages from './modules/lazyImages';
+import documentReady from './helpers/documentReady';
+
+import test from './test';
+
+documentReady(() => {
+  lazyImages();
+  test();
+});
+
+
